45acc7e4db709a0b1d8cd1907a8fbaed60eccdb8
AntonyIl <russiafunbaby@mail.ru>
2024-06-18 00:21:34 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+e = 'love puppa'
+print(len(e))
+
+first = 10
+second = 5
+print(first + second)
+print(first - second)
+summa = (15)
+diff = (5)
+print(summa, diff)
+third = 7
+print((first + second + third) / 3)
+first_string = ('Vtornic')
+second_string = ('Ponedelnic')
+print(second_string, ', ', first_string)
+a= (1)
+b= (2)
+c= (3)
+print(a * c)
+print(a * b)
+f = (5)
+print(5 ** 3 // 2)
