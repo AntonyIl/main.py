862242bedc9f0d836d96fdd23598dd8ecd601350
AntonyIl <russiafunbaby@mail.ru>
2024-06-21 10:10:12 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+a = 12                #kolichstvo_vipolnenih_D.Z.
+b = 1.5                #kolichestvo_zatrachenih_chasov
+c = 'Python'                #nazvanie_cursa
+x = (a/b)            #vremia_zatrachenoe_na_odno_zadanianie
+print(x)
+print('Курс: Python, ''Количество выполненных задач:' ,a,',' 'затрачено часов:' ,b,','' среднее время выполнения задания:',x,)
