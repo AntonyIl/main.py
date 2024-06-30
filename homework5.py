9349193304f7e9ff65e0820b25b63fbc867ebc75
AntonyIl <russiafunbaby@mail.ru>
2024-06-30 22:54:08 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,10 @@
+immutable_var = 1 , 2 , True , 'string'
+print(immutable_var)
+#tuple_1 = (1 , 2 , 'dog', [10 , 12])
+#tuple_1[3][0] = 12
+#print(tuple_1)
+#immutable_var[0] = 2
+# для изменения данного типа переменной его необходимо перевести в тип список . пример : tuple_1
+mutable_list = [1 , 2 , False , 'winter']
+mutable_list[2] = True
+print(mutable_list)
